e5c11fc4f57e960a06388f77da14f12068b5ba20
KazMalhotra <kazm31416@gmail.com>
2020-05-03 14:09:11 -0700

    clean up

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const prefix = '='
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
+
 
 
 
@@ -25,10 +28,59 @@ var quotes = [
   "I don't make up the rules, I just enforce them -Steven Michael Pape, making up the rules"
 ]
 
+/*
+let member = message.mentions.members.first() || message.member,
+  user = member.user;
+
+let embed = new Discord.RichEmbed()
+  .addField('Roles:', member.roles.map(r => `${r}`).join(' | '), true)
+*/
 
 
-
+//commands
 client.on("message", msg => {
+
+
+  // Exit and stop if it's not there
+  if (!msg.content.startsWith(prefix)) return;
+
+  //steve quoter 2000
+
+  if (msg.content.startsWith(prefix + "quotes")) {
+    msg.reply(quotes)
+  }
+
+  if (msg.content === (prefix + "quote")) {
+    msg.reply(quotes[Math.floor(Math.random() * quotes.length)])
+  }
+
+  if (msg.content.startsWith(prefix + "help")) {
+    msg.reply("Hi, I'm Steve the bot (not to be confused with Steve the human) and I'm here to moderate this server. If you have any questions, feel free to @Kaz and ask him (he is my developer). If you would like to contrubute, great! I am open source at https://github.com/KazMalhotra/discordbot.")
+  }
+
+
+/*
+  //steve quoter 1000
+  if (msg.content === "=quote") {
+    msg.reply(quotes[Math.floor(Math.random() * quotes.length)])
+  }
+  if (msg.content === "=quotes") {
+    msg.reply(quotes)
+  }
+
+
+  if (msg.content === "=help") {
+    msg.reply("Hi, I'm Steve the bot (not to be confused with Steve the human) and I'm here to moderate this server. If you have any questions, feel free to @Kaz and ask him (he is my developer). If you would like to contrubute, great! I am open source at https://github.com/KazMalhotra/discordbot.")
+  }
+*/
+
+
+})
+
+
+//other random stuff
+client.on("message", msg => {
+
   if (msg.content === "-cat" || msg.content === "-CatFact" || msg.content === "-catfacts" || msg.content === "-dog" || msg.content === "-cf") {
     sleep(200)
     msg.reply("citation needed")
@@ -38,20 +90,6 @@ client.on("message", msg => {
     sleep(200)
     msg.reply("should chill")
   }
-  //steve quoter 1000
-  if (msg.content === "=quote") {
-    msg.reply(quotes[Math.floor(Math.random() * quotes.length)])
-  }
-  //steve quoter 1000
-  if (msg.content === "=quotes") {
-    msg.reply(quotes)
-  }
-
-  if (msg.content === "=help") {
-    sleep(200)
-    msg.reply("Hi, I'm Steve the bot (not to be confused with Steve the human) and I'm here to moderate this server. If you have any questions, feel free to @Kaz and ask him (he is my developer). If you would like to contrubute, great! I am open source at https://github.com/KazMalhotra/discordbot.")
-  }
-
 })
 
 var key = process.env.API_KEY;
